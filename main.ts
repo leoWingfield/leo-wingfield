19d8278e99295b42532df0e053dacc77bfc4d9d7
leoWingfield <66516991+leoWingfield@users.noreply.github.com>
2020-06-06 17:36:06 +0100

    creating portal to next level

--- main.ts
+++ main.ts
@@ -37,8 +37,47 @@ a b a b a b a b b a b a b a b a
 a a b a b a b a a b a b a b a a 
 a a a a a a a a a a a a a a a a 
 `
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+. . . . . . . 5 5 . . . . . . . 
+. . . . . . 5 5 5 5 . . . . . . 
+. . . . . 5 5 4 4 5 5 . . . . . 
+. . . . 5 5 4 4 4 4 5 5 . . . . 
+. . . 5 5 4 4 3 3 4 4 5 5 . . . 
+5 5 5 4 4 4 3 3 3 3 4 4 4 5 5 5 
+. 5 5 4 4 3 3 a a 3 3 4 4 5 5 . 
+. . 5 5 4 4 3 3 3 3 4 4 5 5 . . 
+. . 5 5 5 4 4 3 3 4 4 5 5 . . . 
+. . . 5 5 5 4 4 4 4 5 5 . . . . 
+. . . . . 5 5 4 4 5 5 . . . . . 
+. . . . . . 5 5 5 5 . . . . . . 
+. . . . . . . 5 5 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
 }
-let duck = sprites.create(img`
+function level () {
+    duck = sprites.create(img`
 . . . . . . . . . . b 5 b . . . 
 . . . . . . . . . b 5 b . . . . 
 . . . . . . b b b b b b . . . . 
@@ -56,21 +95,41 @@ b b c c c d d d 5 5 5 5 5 d b .
 . . . . c c d d d 5 5 5 b b . . 
 . . . . . . c c c c c b b . . . 
 `, SpriteKind.Player)
-controller.moveSprite(duck, 100, 0)
-scene.setBackgroundColor(9)
-duck.ay = 200
-tiles.setTilemap(tiles.createTilemap(
-            hex`1400080000000000000000000000000000000000000000000000000000000000000101010101010101010101000000000000000000000000000000000000000001010101010101010100000000000000000000000000000000000000000000000000000000000000000000000000000101010101010101010101010100000000000000000000000000000000000000000101010101010101010101010101010101010101`,
+    controller.moveSprite(duck, 100, 0)
+    scene.setBackgroundColor(9)
+    duck.ay = 200
+    duck.setFlag(SpriteFlag.StayInScreen, true)
+    scene.cameraFollowSprite(duck)
+    levels = 0
+    tiles.setTilemap(tiles.createTilemap(
+            hex`100008000000000000000000000000000000000300000000000000000001010101010101000000000000000000000000000000000101010101010101010000000000000000000000000000000000000000000000000000000000000101010101010101010000000000000000000000000000000001010101010101010101010101010101`,
             img`
-. . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . 2 2 2 2 2 2 2 2 2 2 2 
-. . . . . . . . . . . . . . . . . . . . 
-2 2 2 2 2 2 2 2 2 . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . 
-. . . . . . . 2 2 2 2 2 2 2 2 2 2 2 2 2 
-. . . . . . . . . . . . . . . . . . . . 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . 2 2 2 2 2 2 2 
+. . . . . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . 2 2 2 2 2 2 2 2 2 
+. . . . . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 `,
-            [myTiles.tile0,myTiles.tile1],
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3],
             TileScale.Sixteen
         ))
+}
+scene.onHitWall(SpriteKind.Player, function (sprite) {
+    current_level = 0
+    levels = 0
+})
+scene.onOverlapTile(SpriteKind.Player, myTiles.tile3, function (sprite, location) {
+    game.over(true)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (duck.vy == 0) {
+        duck.vy = -150
+    }
+})
+let current_level = 0
+let levels = 0
+let duck: Sprite = null
+level()
