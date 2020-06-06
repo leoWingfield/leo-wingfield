1dd5f763ffa3daaa040607a90b7c2f1e37e18485
leoWingfield <66516991+leoWingfield@users.noreply.github.com>
2020-06-06 19:12:48 +0100

    CREATING SOUND EFFECTS

--- main.ts
+++ main.ts
@@ -170,6 +170,44 @@ c a 2 2 c c c 4 2 c c c c a 4 2
 2 2 2 a a a c c c 2 2 4 c c c 2 
 c c c c c c 2 2 2 c c c c c c c 
 `
+    //% blockIdentity=images._tile
+    export const tile9 = img`
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f f 5 5 . f f f . . . . . 
+. . f f 5 . . . . . f f . . . . 
+. . f . . . . . . . . f f f . . 
+. f f . . . . . . . . . . f f . 
+. f . . . . . . . . . . . . f f 
+. f . . . . . . . . . . . . . f 
+. f . . . . . . . . . . . . f f 
+. f . . . . . . . . . . . . f . 
+. f f . . . . . . . . . . . f . 
+. . f f . . . . . . . . . f f . 
+. . . f f . . . . . . . . f . . 
+. . . . f f . . . . . . f f . . 
+. . . . . f f f f f f f f . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile10 = img`
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . f f 5 5 5 5 5 f . . . . . 
+. . . f 5 5 4 4 4 5 5 f . . . . 
+. . . f 5 5 5 5 5 5 5 5 f . . . 
+. . . f 5 4 5 5 3 3 5 5 5 f . . 
+. . . f 5 4 5 3 3 3 3 5 5 5 f . 
+. . . f 5 4 5 3 3 3 3 5 5 4 f . 
+. . . f 5 4 5 3 3 3 3 5 5 5 f . 
+. . . f 5 4 5 5 3 3 5 5 5 f . . 
+. . . f 5 5 5 5 5 5 5 4 f . . . 
+. . . f 5 5 5 5 5 5 5 f . . . . 
+. . . f f 5 5 5 4 5 f . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
 }
 function level () {
     if (current_level == 0) {
@@ -185,7 +223,7 @@ function level () {
 . . . . . . . . . . . . . . . . 
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 `,
-            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8],
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10],
             TileScale.Sixteen
         ))
     } else if (current_level == 1) {
@@ -196,30 +234,59 @@ function level () {
 . . . . . . . 2 2 2 2 . . . . . . 2 2 2 
 . . . 2 2 2 . . . . . . 2 . . . 2 . . . 
 . . . . . . . . . . . . . . . 2 . . . . 
-. 2 2 2 2 2 . . . . . . . . . . . . . . 
-. . . . . . . 2 2 2 2 2 . . . . . . . . 
+. 2 2 2 2 2 . . . . . . . . 2 . . . . . 
+. . . . . . . 2 2 2 2 2 2 2 2 . . . . . 
 . . . . . . . . . . . . . . . . . . . . 
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 `,
-            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8],
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10],
             TileScale.Sixteen
         ))
-    } else if (0 == 0) {
+    } else if (current_level == 2) {
         tiles.setTilemap(tiles.createTilemap(
-            hex`2000080000000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000010101010000000000000001010101000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010101010000000000010000000001000000000500000000010101000000000000000000000000000000000000010000000000050000000000000000010101010000000000000000000000010000000000000001010101000000000000000000000000000000000000000000000000000000000808080808080808080808080808080808080808080808080808080808080808`,
+            hex`2000080000000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000010101010000000000000001010101000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010101010000000000010000000001000000000000000000010101000000000000000000000000000000000000010000000000050000000000000000010101010000000000000000000000010000000000000001010101000000000000000000000000000000000000000000000000000000000808080808080808080808080808080808080808080808080808080808080808`,
             img`
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . 2 2 2 2 . . . . . . . 2 2 2 2 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . 2 2 2 2 . . . . . 2 . . . . 2 . . . . 
+. . . . . 2 2 2 . . . . . . . . . . . . . . . . . . 2 . . . . . 
+. . . . . . . . . 2 2 2 2 . . . . . . . . . . . 2 . . . . . . . 
+2 2 2 2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 `,
-            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8],
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10],
             TileScale.Sixteen
         ))
+        game.showLongText("WATCH OUT FOR LAVA OR YOU LOOSE TWO LIVES", DialogLayout.Bottom)
+        info.setLife(5)
+    } else if (current_level == 3) {
+        tiles.setTilemap(tiles.createTilemap(
+            hex`1e0012000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000100000000010000000000000000000000000000000000000000000100000000000000000001000000000000000003000000000000000000000000000000010000000000000000000000010101000000000000000001000100000000000000000000000000000000000000000000000000000000000000010000000000000000000000010101000000000000000000000000000100000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101000000010101000000000001000001000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010000000000000000000000010101010000000000000000000000010101000000000000000000000000000000000000000000000000000000000000000000000000000000000000050000000001010101010000000000000000000000000000000000000000050000000000000000000000000000000000000000000000000000000000010101010100000000000000000000000000000000000000000000000000080808080808080808080808080808080808080808080808080808080808`,
+            img`
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . 2 . . . . 2 . . . . . . . . . . . 
+. . . . . . . . . . 2 . . . . . . . . . 2 . . . . . . . . . 
+. . . . . . . . . . . . . . . 2 . . . . . . . . . . . 2 2 2 
+. . . . . . . . 2 . 2 . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . 2 . . . . . . . . . . . 2 2 2 . . . 
+. . . . . . . . . . 2 . . . . . . . . . . 2 . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . 2 2 2 2 . . . 2 2 2 . . . . . 2 . . 2 . . . . . . 
+. . . 2 . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . 2 2 . . . . . . . . . . . 
+2 2 2 2 . . . . . . . . . . . 2 2 2 . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . 2 2 2 2 2 . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+2 2 2 2 2 . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10],
+            TileScale.Sixteen
+        ))
+        game.showLongText("IF YOU FIND THE COIN YOU GET TWO EXTRA LIVES", DialogLayout.Bottom)
     } else {
     	
     }
@@ -239,11 +306,19 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 scene.onOverlapTile(SpriteKind.Player, myTiles.tile8, function (sprite, location) {
-    game.over(false)
+    tiles.placeOnRandomTile(duck, myTiles.tile5)
+    info.changeLifeBy(-1)
+    music.powerDown.play()
+})
+scene.onOverlapTile(SpriteKind.Player, myTiles.tile10, function (sprite, location) {
+    let COIN: Sprite = null
+    info.changeLifeBy(2)
+    COIN.destroy()
 })
 let levels = 0
 let current_level = 0
 let duck: Sprite = null
+game.splash("GET TO THE PORTAL          GOOD LUCK")
 duck = sprites.create(img`
 . . . . . . . . . . b 5 b . . . 
 . . . . . . . . . b 5 b . . . . 
@@ -264,5 +339,5 @@ b b c c c d d d 5 5 5 5 5 d b .
 `, SpriteKind.Player)
 scene.setBackgroundColor(9)
 controller.moveSprite(duck, 100, 0)
-current_level = 0
+current_level = 3
 level()
